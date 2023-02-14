88683b3e2f8d4ce8fbe2c19776bf3445fa9150fe
temi-t <temiemmanuel@yahoo.com>
2023-02-14 16:28:31 +0100

    test: branch should pass test

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World!</h1>
-        {/* <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a> */}
+        </a>
       </header>
     </div>
   );
